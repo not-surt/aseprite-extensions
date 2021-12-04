bda3b575fd6fb68d4262f61366b36f1e0d3ccef1
surt <carl.olsson@gmail.com>
2021-12-04 19:53:27 +1000

    Palette Utils colours from selection functions.

--- Palette Utils/Palette Utils.lua
+++ Palette Utils/Palette Utils.lua
@@ -12,47 +12,48 @@ function init(plugin)
     local function loadUserData()
     end
 
+    local function coloursInSelection(cels, selection)
+        local colourSequence = {}
+        local colourCount = {}
+        local count = 0
+        for _, cel in ipairs(cels) do
+            local image = cel.image
+            local bounds = Rectangle(0, 0, image.width, image.height)
+            local offsetSelectionBounds = Rectangle(selection.bounds.x - cel.position.x, selection.bounds.y - cel.position.y, selection.bounds.width, selection.bounds.height)
+            bounds = bounds:intersect(offsetSelectionBounds)
+            for it in image:pixels(bounds) do
+                if selection:contains(it.x + cel.position.x, it.y + cel.position.y) then
+                    count = count + 1
+                    local pixelValue = it()
+                    if colourCount[pixelValue] == nil then
+                        colourCount[pixelValue] = 1
+                        colourSequence[#colourSequence + 1] = pixelValue
+                    else
+                        colourCount[pixelValue] = colourCount[pixelValue] + 1
+                    end
+                end
+            end
+        end
+        return colourSequence, colourCount
+    end
+
     local function buildDialog()
         local dlg = Dialog("Palette Utils")
 
-        dlg:separator{text="Palette Switcher"}
-        dlg:combobox{id="palette", label="Palette", options={"Palette 1", "Palette 2"}, option="Palette 1", onchange=function()
-        end}
-        dlg:entry{id="name", label="Name", text="Palette 1", onchange=function()
-        end}
-        dlg:slider{id="paletteSlider", label="Palette", min=1, max=4, value=0, onchange=function()
-        end}
-        dlg:button{text="Save New", onclick=function()
-        end}
-        dlg:button{text="Save Over", onclick=function()
-        end}
-        dlg:button{text="Delete", onclick=function()
-        end}
-        dlg:button{text="Delete All", onclick=function()
-        end}
-        dlg:newrow()
-        dlg:button{text="Previous Palette", onclick=function()
-        end}
-        dlg:button{text="Next Palette", onclick=function()
-        end}
-        dlg:newrow()
-        dlg:button{text="Load from User Data", onclick=function()
-            loadUserData()
-        end}
-        dlg:button{text="Store in User Data", onclick=function()
-            storeUserData()
-        end}
-
         dlg:separator{text="Selection"}
         dlg:check{id="selectPixels", text="Select Pixels", selected=false}
-        dlg:number{id="distanceThreshold", label="Distance Threshold", text=0}
+        dlg:number{id="similarityThreshold", label="Similarity Threshold", text=0}
         dlg:button{text="Select Similar", onclick=function()
         end}
         dlg:number{id="frequencyThreshold", label="Frequency Threshold", text=0}
         dlg:button{text="Select Infrequent", onclick=function()
         end}
-        dlg:number{id="leastUsedCount", label="Least Used Count", text=0}
-        dlg:button{text="Select Least Used", onclick=function()
+        dlg:number{id="nLeastUsed", label="N Least Used", text=0}
+        dlg:button{text="Select N Least Used", onclick=function()
+            local palette = app.activeSprite.palettes[1]
+            for i = 0, #palette - 1 do
+                
+            end
         end}
 
         dlg:separator{text="Merging"}
@@ -66,10 +67,36 @@ function init(plugin)
         dlg:button{text="Merge Selected", onclick=function()
         end}
 
-        dlg:separator{text="Generation"}
-        dlg:button{text="Add All Colours From Selection", onclick=function()
+        dlg:separator{text="Add Colours"}
+        dlg:button{text="All From Selection", onclick=function()
+            local colourSequence, _ = coloursInSelection(app.range.cels, app.activeSprite.selection)
+            local palette = app.activeSprite.palettes[1]
+            local paletteSize = #palette
+            palette:resize(paletteSize + #colourSequence)
+            for i, pixelValue in ipairs(colourSequence) do
+                local colour = Color(pixelValue)
+                palette:setColor(paletteSize - 1 + i, colour)
+            end
         end}
-        dlg:button{text="Add Colour From Selection Average", onclick=function()
+        dlg:newrow()
+        dlg:button{text="Average From Selection", onclick=function()
+            local _, colourCount = coloursInSelection(app.range.cels, app.activeSprite.selection)
+            local sumR, sumG, sumB, sumA = 0, 0, 0, 0
+            local sumCount = 0
+            for pixelValue, count in pairs(colourCount) do
+                local colour = Color(pixelValue)
+                sumR = sumR + colour.red * count
+                sumG = sumG + colour.green * count
+                sumB = sumB + colour.blue * count
+                sumA = sumA + colour.alpha * count
+                sumCount = sumCount + count
+            end
+            -- local averageColour = Color{red = sumR // sumCount, green = sumG // sumCount, blue = sumB // sumCount, alpha = sumA // sumCount}
+            local averageColour = Color{red = math.floor(sumR / sumCount + 0.5), green = math.floor(sumG / sumCount + 0.5), blue = math.floor(sumB / sumCount + 0.5), alpha = math.floor(sumA / sumCount + 0.5)}
+            local palette = app.activeSprite.palettes[1]
+            local paletteSize = #palette
+            palette:resize(paletteSize + 1)
+            palette:setColor(paletteSize, averageColour)
         end}
 
         dlg:separator{text="Misc."}
